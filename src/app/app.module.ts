0a371cc045b3bb0c4f7f71c0c8832e6d52c68a3c
Rob Lowe <rhlowe@gmail.com>
2020-03-08 15:55:22 -0500

    Add `theatre-list` and `film-list` components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TheaterListComponent } from './theater-list/theater-list.component';
+import { FilmListComponent } from './film-list/film-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TheaterListComponent,
+    FilmListComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/theater-list/theater-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'theater-list',
+  templateUrl: './theater-list.component.html',
+  styleUrls: ['./theater-list.component.scss']
+})
+export class TheaterListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
