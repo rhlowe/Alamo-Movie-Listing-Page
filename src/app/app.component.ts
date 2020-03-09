b04b74e68875a32b3eddc5862c1b6a3f513dec6e
Rob Lowe <rhlowe@gmail.com>
2020-03-09 08:04:21 -0500

    Revert "Revert "Data binding and retrieval.""
    This reverts commit ef136aa33e27fe44a81cffb98d1546580fe7a0f8.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,34 @@
-import { AlamoService } from './config/alamo.service'
 import { Component } from '@angular/core';
 
 @Component({
-  providers: [ AlamoService ],
+  providers: [],
   selector: 'app-root',
   styleUrls: ['./app.component.scss'],
   templateUrl: './app.component.html',
 })
 export class AppComponent {
-  data = {};
+  activeTheater = "";
+  cinemas = [];
 
-  constructor(private alamoService: AlamoService) {
-    this.alamoService.getFilmData().subscribe(data => {
-      this.data = data;
-      console.debug('data', data);
+  constructor() {
+    fetch("https://drafthouse.com/s/mother/v1/page/market/main/austin")
+    .then(response => {
+      return response.json();
+    })
+    .then(json => {
+      this.cinemas = json.data.market.cinemas.sort((a, b) => {
+        if (a.name > b.name) {
+          return 1;
+        }
+
+        return -1;
+      });
     });
   }
 
   title = 'Alamo-Movie-Listing-Page';
+
+  updateActiveTheater(event) {
+    this.activeTheater = event;
+  }
 }
--- src/app/theater-list/theater-list.component.ts
+++ src/app/theater-list/theater-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'theater-list',
@@ -6,10 +6,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./theater-list.component.scss']
 })
 export class TheaterListComponent implements OnInit {
+  activeTheater = "";
+
+  @Input() cinemas;
+
+  @Output() updateTheater = new EventEmitter;
 
   constructor() {}
 
   ngOnInit(): void {
   }
 
+  handleSubmit(e) {
+    e.preventDefault();
+  }
+
+  selectTheater(theater) {
+    this.activeTheater = theater;
+    this.updateTheater.emit(this.activeTheater);
+  }
 }
